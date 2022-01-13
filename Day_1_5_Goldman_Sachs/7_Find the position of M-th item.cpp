70fac103f90a4e8e0af8f2905d93bef7ac57f272
Ritik Kanotra <ritikkanotra@gmail.com>
2022-01-13 16:45:51 +0530

    Create 7_Find the position of M-th item.cpp

---
+++ Day_1_5_Goldman_Sachs/7_Find the position of M-th item.cpp
@@ -0,0 +1,37 @@
+// { Driver Code Starts
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+
+class Solution {
+  public:
+    int findPosition(int N , int M , int K) {
+        // code here
+        if (N == 1) {
+            return 1;
+        }
+        M %= N;
+        // M++;
+        int ans = (M + K - 1) % N;
+        return (ans != 0 ? ans : N);
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N,M,K;
+        
+        cin>>N>>M>>K;
+
+        Solution ob;
+        cout << ob.findPosition(N,M,K) << endl;
+    }
+    return 0;
+}  // } Driver Code Ends
