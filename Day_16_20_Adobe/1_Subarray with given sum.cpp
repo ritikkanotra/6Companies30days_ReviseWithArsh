09265c1ba7fa3b22d5b4495f33b563cf6d4d2616
Ritik Kanotra <ritikkanotra@gmail.com>
2022-01-20 21:06:46 +0530

    Create 1_Subarray with given sum.cpp

---
+++ Day_16_20_Adobe/1_Subarray with given sum.cpp
@@ -0,0 +1,60 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to find a continuous sub-array which adds up to a given number.
+    vector<int> subarraySum(int arr[], int n, long long s)
+    {
+        // Your code here
+        int low = 0, high = 0;
+        int sum = arr[0];
+        while (high < n) {
+            if (sum == s) {
+                return {low + 1, high + 1};
+            }
+            else if (sum < s) {
+                sum += arr[++high];
+            }
+            else {
+                sum -= arr[low++];
+            }
+            
+        }
+        
+        return {-1};
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+ {
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        long long s;
+        cin>>n>>s;
+        int arr[n];
+        const int mx = 1e9;
+        for(int i=0;i<n;i++)
+        {
+            cin>>arr[i];
+        }
+        Solution ob;
+        vector<int>res;
+        res = ob.subarraySum(arr, n, s);
+        
+        for(int i = 0;i<res.size();i++)
+            cout<<res[i]<<" ";
+        cout<<endl;
+        
+    }
+	return 0;
+}  // } Driver Code Ends
