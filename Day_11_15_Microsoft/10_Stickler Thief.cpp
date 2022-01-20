41309caf0355022666d42c1763a917b78f3538c3
Ritik Kanotra <ritikkanotra@gmail.com>
2022-01-20 20:27:51 +0530

    Create 10_Stickler Thief.cpp

---
+++ Day_11_15_Microsoft/10_Stickler Thief.cpp
@@ -0,0 +1,48 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+ // } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to find the maximum money the thief can get.
+    int FindMaxSum(int arr[], int n)
+    {
+        // Your code here
+        for (int i = n - 3; i >= 0; i--) {
+            if (i == n - 3) {
+                arr[i] += arr[i + 2];
+            }
+            else {
+                arr[i] += max(arr[i + 2], arr[i + 3]);
+            }
+        }
+        return n == 1 ? arr[0] : max(arr[0], arr[1]);
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+    //taking total testcases
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    //taking number of houses
+		int n;
+		cin>>n;
+		int a[n];
+		
+		//inserting money of each house in the array
+		for(int i=0;i<n;++i)
+			cin>>a[i];
+		Solution ob;
+		//calling function FindMaxSum()
+		cout<<ob.FindMaxSum(a,n)<<endl;
+	}
+	return 0;
+}
+  // } Driver Code Ends
